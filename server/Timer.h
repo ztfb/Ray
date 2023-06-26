904cf2d19cbded9fcf1f153758c1ab9bd54832d2
ztfb <3192436726@qq.com>
2023-06-26 23:48:30 +0800

    定时器文档

--- server/Timer.h
+++ server/Timer.h
@@ -26,7 +26,7 @@ public:
     void add(int id,int timeout,std::function<void()> callback); //添加一个节点
     void clearTimeoutNode(); // 清除所有超时的节点
     int getExpiration(); // 获取距离最近的到期时间的毫秒数
-    void del(int id); // 根据索引删除一个制定的节点
+    void del(int id); // 根据id删除一个节点
 
     Timer(const Timer&) = delete; // 禁用拷贝构造函数
     Timer& operator=(const Timer&) = delete; // 禁用赋值运算符
